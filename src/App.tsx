ff183448b39648b9d015cb6ad65195324221add9
Jonny Nabors <jonnynabors@gmail.com>
2019-04-20 15:23:45 -0400

    Render chatbox; Create custom peer id
    Signed-off-by: Lee Dobryden <lndobryden@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,80 @@
 require("dotenv").config();
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Peer from "peerjs";
 
+interface SimpleMessage {
+  author: string;
+  message: string;
+}
+
 interface State {
   peer: Peer;
+  peerId: string;
   connection: any;
   connectionId: string;
   message: string;
+  videoSource: string;
+  chats: SimpleMessage[];
 }
 class App extends Component<{}, State> {
   constructor(props: any) {
     super(props);
     this.state = {
-      peer: new Peer("jonnynabors", {
+      peer: new Peer("", {
         debug: 3
       }),
+      peerId: "",
       connection: "",
       connectionId: "",
-      message: ""
+      message: "",
+      videoSource: "",
+      chats: []
     };
   }
 
-  componentDidMount() {
-    console.log("peer", this.state.peer);
-    console.log("connection", this.state.connection);
+  createPeer = () => {
+    // TODO: Only create a peer that the user defines a name for
+    // currently in the state declaration, we create one
+    this.setState(
+      {
+        peer: new Peer(this.state.peerId, {
+          debug: 3
+        })
+      },
+      () => {
+        this.state.peer.on("connection", conn => {
+          console.log(conn);
+          console.log(this.state.peer.id);
+          this.setState({
+            peerId: this.state.peer.id
+          });
 
-    this.state.peer.on("connection", conn => {
-      console.log(conn);
-      console.log("connection from peer");
+          conn.on("data", data => {
+            let currentChats = this.state.chats;
+            currentChats.push({
+              author: "Them",
+              message: data
+            });
+            this.setState({
+              chats: currentChats
+            });
+          });
 
-      conn.on("data", data => {
-        // Will print 'hi!'
-        console.log(data);
-      });
-    });
-  }
+          this.state.peer.on("call", async incomingCall => {
+            const mediaStream = await navigator.mediaDevices.getUserMedia({
+              video: false,
+              audio: true
+            });
+            incomingCall.answer(mediaStream);
+            incomingCall.on("stream", remoteStream => {
+              console.log("remote stream", remoteStream);
+            });
+          });
+        });
+      }
+    );
+  };
 
   openConnection = () => {
     this.setState(
@@ -49,8 +87,6 @@ class App extends Component<{}, State> {
 
   connect = () => {
     this.state.connection.on("open", (msg: any) => {
-      console.log("logging with message:", msg);
-
       this.state.connection.send("hi!");
     });
 
@@ -68,29 +104,97 @@ class App extends Component<{}, State> {
   updateMessage = (message: string) => {
     this.setState({
       message
-    })
-  }
+    });
+  };
+
+  updatePeerId = (peerId: string) => {
+    this.setState({ peerId });
+  };
 
   sendMessage = () => {
     this.state.connection.send(this.state.message);
-  }
+    let chats = this.state.chats;
+    chats.push({
+      author: "You",
+      message: this.state.message
+    });
+    this.setState({
+      message: ""
+    });
+  };
+
+  call = async () => {
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: false,
+      audio: true
+    });
+    const audioCall = this.state.peer.call(
+      this.state.connectionId,
+      mediaStream
+    );
+    audioCall.on("stream", remoteStream => {
+      // do stuff!
+      console.log(remoteStream);
+    });
+
+    audioCall.on("error", error => {
+      console.log(error);
+    });
+  };
+
+  renderChatMessages = () => {
+    return this.state.chats.map((chat, index) => (
+      <div
+        key={index}
+        className={chat.author === "Them" ? "server-text" : "you-text"}
+      >
+        {chat.author}: {chat.message}
+      </div>
+    ));
+  };
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="container">
+          <div className="header">
+            <h1>Floo Network</h1>
+            {this.state.peerId ? (
+              <h3>Your Peer Id is {this.state.peerId}</h3>
+            ) : (
+              <h3>Please create a Peer Id</h3>
+            )}
+          </div>
+          <div className="connection">
+            <input
+              type="text"
+              placeholder="Create a Unique Peer Id"
+              onChange={e => this.updatePeerId(e.target.value)}
+            />
+            <button type="button" onClick={this.createPeer}>
+              Create Peer
+            </button>
+            <input
+              type="text"
+              placeholder="Enter Connection Id"
+              onChange={e => this.handleConnectionId(e.target.value)}
+            />
+            <button type="button" onClick={this.openConnection}>
+              Connect
+            </button>
+          </div>
           <input
             type="text"
-            onChange={e => this.handleConnectionId(e.target.value)}
+            onChange={e => this.updateMessage(e.target.value)}
           />
-          <button type="button" onClick={this.openConnection}>
-            Connect
-          </button>
-          <input type="text" onChange={e => this.updateMessage(e.target.value)}></input>
           <button type="button" onClick={this.sendMessage}>
             Send Message
           </button>
-        </header>
+          <button type="button" onClick={this.call}>
+            Call
+          </button>
+          <div className="chatbox">{this.renderChatMessages()}</div>
+        </div>
       </div>
     );
   }
