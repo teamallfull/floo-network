b0bc87c20e950ff5000164b2b607f88d44cbf222
Jonny Nabors <jonnynabors@gmail.com>
2019-06-05 21:13:33 -0400

    Create context for network connection and peer

--- src/connection/ConnectionContext.tsx
+++ src/connection/ConnectionContext.tsx
@@ -1,20 +1,23 @@
-import React, {
-  useEffect,
-  useState,
-  useLayoutEffect,
-  useContext,
-  createContext
-} from "react";
-import Peer from "peerjs";
+import React, { useEffect, useState, createContext } from "react";
+import Peer, { DataConnection } from "peerjs";
 
 type Connection = {
   peer: Peer | null;
-  setPeer: (id: string) => Peer;
+  setPeer: Function;
 };
 export const ConnectionContext = createContext<Connection>({} as Connection);
 
 export function ConnectionProvider(props: any) {
-  const [peer, setPeer] = useState(null);
+  const [peer, setPeer] = useState<Peer | null>(null);
+
+  if (peer) {
+    peer.on("open", id => console.log(`Opening new peer with id: ${id}`));
+    peer.on("connection", dataConnection =>
+      console.log("connected with details ", dataConnection)
+    );
+  }
+
+  console.log("context");
 
   return (
     <ConnectionContext.Provider
