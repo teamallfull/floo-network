21d3587d82fdf7e79ac3e00bcf55abcb9c5f6aae
Jonny Nabors <jonnynabors@gmail.com>
2019-06-05 21:40:09 -0400

    Conditionally render connect options if peer has been created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 require("dotenv").config();
-import React, { Component } from "react";
+import React, { Component, useContext, useEffect } from "react";
 import "./App.css";
 import Peer from "peerjs";
-import { ConnectionProvider } from "./connection/ConnectionContext";
+import {
+  ConnectionProvider,
+  ConnectionContext
+} from "./connection/ConnectionContext";
 import { Connection } from "./connection/Connection";
 
 interface SimpleMessage {
@@ -10,181 +13,36 @@ interface SimpleMessage {
   message: string;
 }
 
-interface State {
-  peer: Peer;
-  peerId: string;
-  connection: any;
-  connectionId: string;
-  message: string;
-  videoSource: string;
-  chats: SimpleMessage[];
-}
-class App extends Component<{}, State> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      peer: new Peer("", {
-        debug: 3
-      }),
-      peerId: "",
-      connection: "",
-      connectionId: "",
-      message: "",
-      videoSource: "",
-      chats: []
-    };
-  }
-
-  createPeer = () => {
-    // TODO: Only create a peer that the user defines a name for
-    // currently in the state declaration, we create one
-    this.setState(
-      {
-        peer: new Peer(this.state.peerId, {
-          debug: 3
-        })
-      },
-      () => {
-        this.state.peer.on("connection", conn => {
-          console.log(conn);
-          console.log(this.state.peer.id);
-          this.setState({
-            peerId: this.state.peer.id
-          });
-
-          conn.on("data", data => {
-            let currentChats = this.state.chats;
-            currentChats.push({
-              author: "Them",
-              message: data
-            });
-            this.setState({
-              chats: currentChats
-            });
-          });
-
-          this.state.peer.on("call", async incomingCall => {
-            const mediaStream = await navigator.mediaDevices.getUserMedia({
-              video: false,
-              audio: true
-            });
-            incomingCall.answer(mediaStream);
-            incomingCall.on("stream", remoteStream => {
-              console.log("remote stream", remoteStream);
-            });
-          });
-        });
-      }
-    );
-  };
-
-  openConnection = () => {
-    this.setState(
-      {
-        connection: this.state.peer.connect(this.state.connectionId)
-      },
-      this.connect
-    );
-  };
-
-  connect = () => {
-    this.state.connection.on("open", (msg: any) => {
-      this.state.connection.send("hi!");
-    });
-
-    this.state.connection.on("error", (error: any) => {
-      console.log(error);
-    });
-  };
-
-  handleConnectionId = (connectionId: string) => {
-    this.setState({
-      connectionId
-    });
-  };
-
-  updateMessage = (message: string) => {
-    this.setState({
-      message
-    });
-  };
-
-  updatePeerId = (peerId: string) => {
-    this.setState({ peerId });
-  };
-
-  sendMessage = () => {
-    this.state.connection.send(this.state.message);
-    let chats = this.state.chats;
-    chats.push({
-      author: "You",
-      message: this.state.message
-    });
-    this.setState({
-      message: ""
-    });
-  };
-
-  call = async () => {
-    const mediaStream = await navigator.mediaDevices.getUserMedia({
-      video: false,
-      audio: true
-    });
-    const audioCall = this.state.peer.call(
-      this.state.connectionId,
-      mediaStream
-    );
-    audioCall.on("stream", remoteStream => {
-      // do stuff!
-      console.log(remoteStream);
-    });
-
-    audioCall.on("error", error => {
-      console.log(error);
-    });
-  };
-
-  renderChatMessages = () => {
-    return this.state.chats.map((chat, index) => (
-      <div
-        key={index}
-        className={chat.author === "Them" ? "server-text" : "you-text"}
-      >
-        {chat.author}: {chat.message}
-      </div>
-    ));
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <ConnectionProvider>
-          <div className="container">
-            <div className="header">
-              <h1>Floo Network</h1>
-              {this.state.peerId ? (
-                <h3>Your Peer Id is {this.state.peerId}</h3>
-              ) : (
-                <h3>Please create a Peer Id</h3>
-              )}
-            </div>
-            <Connection />
-            <input
-              type="text"
-              onChange={e => this.updateMessage(e.target.value)}
-            />
-            <button type="button" onClick={this.sendMessage}>
-              Send Message
-            </button>
-            <button type="button" onClick={this.call}>
-              Call
-            </button>
-            <div className="chatbox">{this.renderChatMessages()}</div>
+function App(props: any) {
+  const context = useContext(ConnectionContext);
+  useEffect(() => {
+    console.log("effecting");
+  }, [context.peer]);
+  return (
+    <div className="App">
+      <ConnectionProvider>
+        <div className="container">
+          <div className="header">
+            <h1>Floo Network</h1>
+            {context.peer ? (
+              <h3>Your Peer Id is {context.peer.id}</h3>
+            ) : (
+              <h3>Please Create a Peer</h3>
+            )}
           </div>
-        </ConnectionProvider>
-      </div>
-    );
-  }
+          <Connection />
+          <input type="text" onChange={e => () => {}} />
+          <button type="button" onClick={() => {}}>
+            Send Message
+          </button>
+          <button type="button" onClick={() => {}}>
+            Call
+          </button>
+          <div className="chatbox" />
+        </div>
+      </ConnectionProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/connection/Connection.tsx
+++ src/connection/Connection.tsx
@@ -1,34 +1,45 @@
 import { ConnectionContext } from "./ConnectionContext";
 import React, { useContext, useState, useEffect } from "react";
+import Peer from "peerjs";
 
 export function Connection() {
   const { setPeer, peer } = useContext(ConnectionContext);
-  const [localPeerId, setLocalPeerId] = useState("");
+  const [peerId, setPeerId] = useState("");
+  const [peerCreated, setPeerCreated] = useState(false);
   const [connectionId, setConnectionId] = useState("");
 
+  useEffect(() => {
+    if (peer && peer.id) {
+      setPeerCreated(true);
+    } else setPeerCreated(false);
+  }, [peer]);
   return (
     <div className="connection">
       <input
         type="text"
         placeholder="Create a Unique Peer Id"
-        onChange={e => setLocalPeerId(e.target.value)}
+        onChange={e => setPeerId(e.target.value)}
       />
       <button
         type="button"
         onClick={() => {
-          setPeer(() => new Peer(localPeerId, { debug: 3 }));
+          setPeer(() => new Peer(peerId, { debug: 3 }));
         }}
       >
         Create Peer
       </button>
-      <input
-        type="text"
-        placeholder="Enter Connection Id"
-        onChange={e => setConnectionId(e.target.value)}
-      />
-      <button type="button" onClick={() => peer!.connect(connectionId)}>
-        Connect
-      </button>
+      {peerCreated && (
+        <>
+          <input
+            type="text"
+            placeholder="Enter Connection Id"
+            onChange={e => setConnectionId(e.target.value)}
+          />
+          <button type="button" onClick={() => peer!.connect(connectionId)}>
+            Connect
+          </button>
+        </>
+      )}
     </div>
   );
 }
